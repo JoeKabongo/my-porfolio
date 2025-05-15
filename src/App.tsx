5351b5cc97cc500ea8637a52bd345b05f43093cb
JoeKabongo <jonathankabongo23@gmail.com>
2025-05-15 01:09:16 -0700

    remove navbar for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
 import Hero from './components/Hero'
-import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 
 const App = () => {
   return (
     <div className="">
-      <Navbar />
       <Hero />
       <About />
       <Projects />
