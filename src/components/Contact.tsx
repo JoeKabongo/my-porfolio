351fe3b6e1f6d7aaaecb96cf7cf7ffc05513f8b1
JoeKabongo <jonathankabongo23@gmail.com>
2025-05-15 01:01:24 -0700

    add html to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <section>
+      <h1> Contact</h1>
+      <p> Want to work in me? Please do not hesitate to reach out!</p>
+      <span> Linkedin </span> <span> Email</span>
+    </section>
+  )
 }
 
 export default Contact
